92439fb0e2d6f3b321e200a4b88c3b3d7d5d3684
fkbeys <fkbeys@gmail.com>
2023-06-14 22:31:34 +0200

    polly retry mechanism added

--- RabbitMqExample.Common/Services/MessageService.cs
+++ RabbitMqExample.Common/Services/MessageService.cs
@@ -1,4 +1,5 @@
-﻿using RabbitMQ.Client;
+﻿using Polly;
+using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMqExample.Common.Models;
 using System.Text;
@@ -11,6 +12,7 @@ namespace RabbitMqExample.Common.Services
         private readonly IModel _channel;
         private readonly IConnection _connection;
         private readonly IRabbitMqSettings _rabbitMqSettings;
+        private readonly Policy _retryPolicy;
 
         protected override string queueName() => typeof(T).Name + "Queue";
         protected override string exchangeName() => typeof(T).Name + "Exchange";
@@ -18,48 +20,62 @@ namespace RabbitMqExample.Common.Services
         public MessageService(IRabbitMqSettings rabbitMqSettings)
         {
             _rabbitMqSettings = rabbitMqSettings;
+            _retryPolicy = Policy
+                .Handle<Exception>()
+                .WaitAndRetry(new[]
+                {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(10)
+                });
             (_connection, _channel) = Connect();
         }
 
         protected override (IConnection, IModel) Connect()
         {
-            var factory = new ConnectionFactory
+            return _retryPolicy.Execute(() =>
             {
-                HostName = _rabbitMqSettings.Host,
-                Port = _rabbitMqSettings.Port,
-                UserName = _rabbitMqSettings.UserName,
-                Password = _rabbitMqSettings.Password
-            };
-            var connection = factory.CreateConnection();
-            var channel = connection.CreateModel();
-            channel.QueueDeclare(queueName(), durable: true, exclusive: false);
-            return (connection, channel);
+                var factory = new ConnectionFactory
+                {
+                    HostName = _rabbitMqSettings.Host,
+                    Port = _rabbitMqSettings.Port,
+                    UserName = _rabbitMqSettings.UserName,
+                    Password = _rabbitMqSettings.Password
+                };
+                var connection = factory.CreateConnection();
+                var channel = connection.CreateModel();
+                channel.QueueDeclare(queueName(), durable: true, exclusive: false);
+                return (connection, channel);
+            });
         }
 
         public override void ReceiveMessage(Action<T> callback)
         {
-            var consumer = new EventingBasicConsumer(_channel);
-
-            consumer.Received += (sender, args) =>
+            _retryPolicy.Execute(() =>
             {
-                var body = args.Body.ToArray();
-                var message = Encoding.UTF8.GetString(body);
-                T data = JsonSerializer.Deserialize<T>(message);
+                var consumer = new EventingBasicConsumer(_channel);
 
-                callback(data);
-            };
+                consumer.Received += (sender, args) =>
+                {
+                    var body = args.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    T data = JsonSerializer.Deserialize<T>(message);
 
-            _channel.BasicConsume(queue: queueName(), autoAck: true, consumer: consumer);
+                    callback(data);
+                };
+
+                _channel.BasicConsume(queue: queueName(), autoAck: true, consumer: consumer);
+            });
         }
 
-         
         public override void SendMessage(T message)
         {
-            var jsonString = JsonSerializer.Serialize(message);
-            var body = Encoding.UTF8.GetBytes(jsonString);
-            //_channel.BasicPublish("", queueName(), null, body);
-            _channel.BasicPublish(exchange: "", routingKey: queueName(), basicProperties: null, body: body);
-
+            _retryPolicy.Execute(() =>
+            {
+                var jsonString = JsonSerializer.Serialize(message);
+                var body = Encoding.UTF8.GetBytes(jsonString);
+                _channel.BasicPublish(exchange: "", routingKey: queueName(), basicProperties: null, body: body);
+            });
         }
 
         public void Dispose()
@@ -67,8 +83,6 @@ namespace RabbitMqExample.Common.Services
             _channel.Dispose();
             _connection.Dispose();
         }
-
-       
     }
 
 }
